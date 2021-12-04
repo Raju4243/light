a876525d3c6447b870d9a75df8256d762dc105e0
Raju4243 <raju.ee.1723@gmail.com>
2021-12-04 10:47:56 +0600

    light turn on and turrn off

---
+++ light.js
@@ -0,0 +1,9 @@
+function TurnOn(){
+    var div=document.getElementById('container');
+    div.src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmR2ORxLHSS9sdgTnFknsynustNu-PRwYtxA&usqp=CAU";
+}
+function TurnOff(){
+    var div=document.getElementById('container');
+    div.src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtCXzYiTfahprfze93evAJyHAmIwuUpow0zA&usqp=CAU";
+    
+}
